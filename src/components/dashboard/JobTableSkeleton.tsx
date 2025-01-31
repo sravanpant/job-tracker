7c15d44ed7166d916b9c335c5fc590b5af4d8804
sravanpant <sravan.pant5@gmail.com>
2025-01-31 16:28:47 +0530

    Added loading skeleton

---
+++ src/components/dashboard/JobTableSkeleton.tsx
@@ -0,0 +1,103 @@
+// src/components/dashboard/JobTableSkeleton.tsx
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export function JobTableSkeleton() {
+  return (
+    <div className="space-y-4">
+      {/* Header Stats Skeleton */}
+      <div className="grid gap-4 md:grid-cols-5">
+        {Array.from({ length: 5 }).map((_, i) => (
+          <div
+            key={i}
+            className="rounded-lg border p-4 space-y-3"
+          >
+            <Skeleton className="h-4 w-[100px]" />
+            <Skeleton className="h-8 w-[60px]" />
+          </div>
+        ))}
+      </div>
+
+      {/* Table Skeleton */}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[30px]">
+                <Skeleton className="h-4 w-4" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[100px]" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[100px]" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[100px]" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[80px]" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[80px]" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[60px]" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-[100px]" />
+              </TableHead>
+              <TableHead className="w-[30px]" />
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <Skeleton className="h-4 w-4" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-[150px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-[200px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-[120px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-6 w-[80px] rounded-full" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-[100px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-[40px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-[100px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-8 w-8 rounded-full" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      {/* Pagination Skeleton */}
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Skeleton className="h-8 w-20" />
+        <Skeleton className="h-8 w-20" />
+      </div>
+    </div>
+  );
+}
